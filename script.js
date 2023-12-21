d4c50dd3777921f713c5398e97c4bb70e47c01d3
Salim GOMRI <salim.gomri@gmail.com>
2023-12-21 14:48:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,52 @@ const candidatures = [
   // Ajoute d'autres données si nécessaire
 ];
 
+function ouvrirFormulaire(candidature) {
+  const form = document.createElement('form');
+  form.innerHTML = `
+    <label for="entreprise">Nom de l'entreprise:</label>
+    <input type="text" id="entreprise" name="entreprise" value="${candidature ? candidature.entreprise : ''}"><br><br>
+    <label for="poste">Poste:</label>
+    <input type="text" id="poste" name="poste" value="${candidature ? candidature.poste : ''}"><br><br>
+    <label for="date">Date de candidature:</label>
+    <input type="text" id="date" name="date" value="${candidature ? candidature.date : ''}"><br><br>
+    <label for="suivi">Suivi:</label>
+    <input type="text" id="suivi" name="suivi" value="${candidature ? candidature.suivi : ''}"><br><br>
+    <label for="etape">Réponse/Étape suivante:</label>
+    <input type="text" id="etape" name="etape" value="${candidature ? candidature.etape : ''}"><br><br>
+    <button id="submit-btn" class="btn btn-primary">${candidature ? 'Modifier' : 'Ajouter'}</button>
+  `;
+
+  const newWindow = window.open('', 'Candidature', 'width=400,height=400');
+  newWindow.document.body.appendChild(form);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const nouvelObjet = {};
+    formData.forEach((value, key) => {
+      nouvelObjet[key] = value;
+    });
+    if (candidature) {
+      candidatures[candidature.index] = nouvelObjet;
+    } else {
+      candidatures.push(nouvelObjet);
+    }
+    newWindow.close();
+    afficherCandidatures();
+  });
+}
+
+function editCandidature(event) {
+  const index = event.target.closest('tr').getAttribute('data-index');
+  const candidature = candidatures[index];
+  candidature.index = index;
+  ouvrirFormulaire(candidature);
+}
+
+function ajouterNouvelleLigne() {
+  ouvrirFormulaire(null);
+}
 
 function afficherCandidatures() {
   const tableBody = document.getElementById('table-body');
@@ -27,49 +73,15 @@ function afficherCandidatures() {
 
   tableBody.innerHTML = html;
 
-  const btnsEdit = document.querySelectorAll('.btn-edit');
+const btnsEdit = document.querySelectorAll('.btn-edit');
   btnsEdit.forEach(btn => {
     btn.addEventListener('click', editCandidature);
   });
 
-  const btnsDelete = document.querySelectorAll('.btn-delete');
+const btnsDelete = document.querySelectorAll('.btn-delete');
   btnsDelete.forEach(btn => {
     btn.addEventListener('click', deleteCandidature);
   });
-}
-
-function ajouterNouvelleLigne() {
-  // Afficher une fenêtre modale (popup) pour saisir les informations
-  const nouvelleLigne = prompt('Entrez les informations séparées par des virgules : Nom Entreprise, Poste, Date, Suivi, Étape suivante');
-  if (nouvelleLigne) {
-    const infos = nouvelleLigne.split(',').map(info => info.trim());
-    const nouvelleCandidature = {
-      entreprise: infos[0],
-      poste: infos[1],
-      date: infos[2],
-      suivi: infos[3],
-      etape: infos[4]
-    };
-    candidatures.push(nouvelleCandidature);
-    afficherCandidatures();
-  }
-}
-
-function editCandidature(event) {
-  const index = event.target.closest('tr').getAttribute('data-index');
-  const candidature = candidatures[index];
-  const nouvelleLigne = prompt('Entrez les nouvelles informations séparées par des virgules : Nom Entreprise, Poste, Date, Suivi, Étape suivante', `${candidature.entreprise}, ${candidature.poste}, ${candidature.date}, ${candidature.suivi}, ${candidature.etape}`);
-  if (nouvelleLigne) {
-    const infos = nouvelleLigne.split(',').map(info => info.trim());
-    candidatures[index] = {
-      entreprise: infos[0],
-      poste: infos[1],
-      date: infos[2],
-      suivi: infos[3],
-      etape: infos[4]
-    };
-    afficherCandidatures();
-  }
 }
 
 function deleteCandidature(event) {
